24e8eddeb0cf8809ae33d5f629c4bd10fddfa047
Seba <259813@student.pwr.edu.pl>
2026-03-08 23:31:05 +0100

    Remove obsolete PyPep3 implementation file

--- src/pycea/__init__.py
+++ src/pycea/__init__.py
@@ -1,7 +1,6 @@
 # src/pycea/__init__.py
 __version__ = "0.1.0"
 
-from .PyPep import *
 from .core import CEA, Species, Results, CombustionEquilibrium, EquilibriumResults
 
 # Control what "from pycea import *" exposes
