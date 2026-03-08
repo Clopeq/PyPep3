24c67f4b2bb648f1186fa01a582254747ca44275
Seba <259813@student.pwr.edu.pl>
2026-03-08 23:35:00 +0100

    Upload pycea package

--- src/pycea/__init__.py
+++ src/pycea/__init__.py
@@ -1,14 +1,12 @@
 # src/pycea/__init__.py
 __version__ = "0.1.0"
 
-from .core import CEA, Species, Results, CombustionEquilibrium, EquilibriumResults
+from .core import CEA, Species, Results
 
 # Control what "from pycea import *" exposes
 __all__ = [
     "CEA",
     "Species", 
     "Results",
-    "CombustionEquilibrium",
-    "EquilibriumResults",
 ]
 
--- src/pycea/core/__init__.py
+++ src/pycea/core/__init__.py
@@ -1,12 +1,9 @@
 """PyPep core module for combustion and thermodynamic calculations."""
 
-from .combustion_equilibrium import CombustionEquilibrium, EquilibriumResults
-from .rocket_cea import CEA, Species, Results
+from .pycea import CEA, Species, Results
 from .constants import ELEMENTAL_MOLAR_MASS
 
 __all__ = [
-    'CombustionEquilibrium',
-    'EquilibriumResults',
     'CEA',
     'Species',
     'Results',
